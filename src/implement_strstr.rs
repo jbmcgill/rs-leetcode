ab62312b6db26c0cc092689fb18363fb315e9890
James McGill <jbmcgill@gmail.com>
2019-01-21 21:29:15 -0800

    implement_strstr

---
+++ src/implement_strstr.rs
@@ -0,0 +1,22 @@
+pub mod solution {
+
+  pub fn str_str( haystack: String, needle: String ) -> i32 {
+    let nlen = needle.len();
+    for (hidx, hval) in haystack.chars().enumerate() {
+      if haystack[hidx..(hidx+nlen)] == needle {
+        return hidx as i32;
+      }
+    }
+    0 
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use crate::implement_strstr::solution;
+  #[test]
+  pub fn test_str_str() {
+    assert!(3 == solution::str_str(String::from("foobar"), String::from("bar")));
+    assert!(10 == solution::str_str(String::from("the quick brown fox"), String::from("brown")));
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod palindrome_number;
 pub mod remove_duplicates_from_sorted_array;
 pub mod remove_element;
 pub mod search_insert_position;
+pub mod implement_strstr;
